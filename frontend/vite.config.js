7b19c6157a2a4388473bdddd27f1da52e6546148
Landscape Architect <hans@landscapearchitect.nl>
2025-07-23 14:13:31 +0200

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,14 +17,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-        },
-      },
-    },
   },
 })
 
